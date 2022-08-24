436c7a3a3cac0075f84e0b0464395436bf0ae613
Nileshkumar Shegokar <nilesh.shegokar@gmail.com>
2022-08-24 22:34:45 +0530

    inital code commit

---
+++ src/test/java/com/ns/AutoNRT/AutoNrtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ns.AutoNRT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoNrtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
